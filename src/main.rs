6d2093901ac2fa90f1518650badba0834a4fd531
Kristian <49764796+kristianka@users.noreply.github.com>
2023-05-15 12:47:31 +0300

    add delete by description

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chrono::NaiveDate;
 use std::env;
-use std::fs::OpenOptions;
-use std::io::{BufWriter, Write};
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
 use std::io::Error;
 use std::path::PathBuf;
@@ -131,6 +131,59 @@ fn read_csv_file() -> Result<CsvData, Error> {
     })
 }
 
+fn delete_an_event(
+    events_path: &PathBuf,
+    temp_path: &PathBuf,
+    line_to_delete: String,
+    event: &Event,
+) {
+    match File::open(&events_path) {
+        Ok(file) => {
+            let reader = BufReader::new(file);
+            // Open the temporary file for writing
+            match OpenOptions::new().write(true).create(true).open(&temp_path) {
+                Ok(file) => {
+                    let mut writer = BufWriter::new(file);
+
+                    // Add lines to new file that don't contain given event
+                    for line in reader.lines() {
+                        if let Ok(line) = line {
+                            if !line.contains(line_to_delete.as_str()) {
+                                writeln!(writer, "{}", line).unwrap();
+                            }
+                        }
+                    }
+
+                    // Flush the buffer to write the changes to disk
+                    writer.flush().unwrap();
+
+                    // Rename temporary events.csv.tmp to overwrite the original file
+                    std::fs::rename(&temp_path, &events_path).unwrap();
+                    println!(
+                        "Successfully deleted event {}: {} ({})",
+                        event.date, event.description, event.category
+                    );
+                }
+                Err(e) => {
+                    eprintln!("Error opening file: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+        }
+    }
+}
+
+fn csv_format_to_event(event: &Event) -> String {
+    format!(
+        "{},{},{}",
+        event.date.format("%Y-%m-%d"),
+        event.category,
+        event.description
+    )
+}
+
 /// Removes commas from a string and makes it into a vector
 fn separate_args_to_vector(args: &String) -> Vec<String> {
     let separated_args: Vec<String> = args.split(",").map(|s| s.to_string()).collect();
@@ -140,6 +193,38 @@ fn separate_args_to_vector(args: &String) -> Vec<String> {
 // Surpressed unsused warnings
 #[allow(unused)]
 fn main() {
+    // Current time
+    let now = chrono::Utc::now().naive_utc().date();
+    // Arguments to vector
+    let args: Vec<String> = env::args().collect();
+    //println!("The arguments are: {:?} ja pituus {}", args, args.len());
+
+    // Counter for found events
+    let mut counter = 0;
+
+    if args.len() == 1 {
+        println!("No arguments entered. Use --help for help.");
+        process::exit(0);
+    }
+
+    let arg_help = "--help";
+    if args[1] == arg_help {
+        println!("Help for the list command:");
+        println!("Usage: days [list] [options]");
+        println!("Options:");
+        println!("--today");
+        println!("--before-date <date>");
+        println!("--after-date <date>");
+        println!("--date <date>");
+        println!("--categories <category1,category2>");
+        println!("--exclude <category1,category2>");
+        println!("--no-category");
+        println!("--add <date> <category> <description>");
+        println!("--remove <date> <category> <description>");
+        println!("--help");
+        counter += 1;
+    }
+
     let mut events_path = PathBuf::new();
     let mut temp_path = PathBuf::new();
     let mut events_vector = Vec::new();
@@ -156,11 +241,6 @@ fn main() {
         }
     }
 
-    // Current time
-    let now = chrono::Utc::now().naive_utc().date();
-    let args: Vec<String> = env::args().collect();
-    //println!("The arguments are: {:?} ja pituus {}", args, args.len());
-
     let arg_list = "list";
     let arg_today = "--today";
     let arg_before_date = "--before-date";
@@ -173,11 +253,12 @@ fn main() {
     // let arg_add = "--add";
 
     // Arguments starting with list
-    if args[1] == arg_list {
+    if args.len() > 1 && args[1] == arg_list {
         // List all events
         if args.len() == 2 {
             for e in events_vector.iter() {
                 print_day_format(&e);
+                counter += 1;
             }
         }
 
@@ -186,6 +267,7 @@ fn main() {
             for e in events_vector.iter() {
                 if e.date == now {
                     print_day_format(&e);
+                    counter += 1;
                 }
             }
         }
@@ -228,22 +310,25 @@ fn main() {
                 if before {
                     if e.date < date1.unwrap() {
                         print_day_format(&e);
+                        counter += 1;
                     }
                 }
                 if after {
                     if e.date > date2.unwrap() {
                         print_day_format(&e);
+                        counter += 1;
                     }
                 }
                 if on_this_date {
                     if e.date == date1.unwrap() {
                         print_day_format(&e);
+                        counter += 1;
                     }
                 }
             }
         }
 
-        // Categories
+        // Categories, still inside list
         // if argument after list is --categories
         if args.len() > 2 && (args[2] == arg_categories || args[2] == arg_no_category) {
             // Print events with no categories
@@ -251,6 +336,7 @@ fn main() {
                 for e in events_vector.iter() {
                     if e.category == "" {
                         print_day_format(&e);
+                        counter += 1;
                     }
                 }
             } else {
@@ -269,10 +355,12 @@ fn main() {
                     if exclude {
                         if !arg_categories.contains(&event.category) {
                             print_day_format(&event);
+                            counter += 1;
                         }
                     } else {
                         if arg_categories.contains(&event.category) {
                             print_day_format(&event);
+                            counter += 1;
                         }
                     }
                 }
@@ -286,7 +374,8 @@ fn main() {
     let mut category = "";
     let mut description = "";
 
-    if args[1] == arg_add {
+    // Arguments starting with add
+    if args.len() > 1 && args[1] == arg_add {
         let mut date_given: bool = true;
 
         if args.len() < 3 {
@@ -316,13 +405,7 @@ fn main() {
         }
 
         let event = Event::new(date.unwrap(), category.to_string(), description.to_string());
-        let event_formatted = format!(
-            "{},{},{}",
-            event.date.format("%Y-%m-%d"),
-            event.category,
-            event.description
-        );
-        println!("{}", event_formatted);
+        let event_formatted = csv_format_to_event(&event);
 
         match OpenOptions::new().append(true).open(&events_path) {
             Ok(file) => {
@@ -332,11 +415,50 @@ fn main() {
                     "Successfully added event {}: {} ({})",
                     event.date, event.description, event.category
                 );
+                counter += 1;
             }
             Err(e) => {
                 eprintln!("Error opening file: {}", e);
             }
         }
     }
+
+    let arg_delete = "delete";
+    let arg_dry_run = "--dry-run";
+
+    // Arguments starting with delete
+    if args.len() > 1 && args[1] == arg_delete {
+        if args.len() < 3 {
+            eprintln!("No date or enough arguments given.\n");
+            process::exit(1);
+        }
+
+        let length = args.len() - 1;
+
+        // If --description is given as first argument after delete
+        if args[2] == arg_description {
+            for event in events_vector.iter() {
+                if event.description.starts_with(&args[3]) {
+                    // Check for dry-run
+                    if args.len() > 4 && args[4] == arg_dry_run {
+                        println!(
+                            "{}: {} ({}) would have been deleted without dry-run",
+                            event.date, event.category, event.description
+                        );
+                    // Delete events for real if dry-run not given
+                    } else {
+                        let event_formatted = csv_format_to_event(&event);
+                        delete_an_event(&events_path, &temp_path, event_formatted, event);
+                    }
+                    counter += 1;
+                }
+            }
+        }
+    }
+
+    if counter == 0 {
+        println!("No events found");
+    }
+    // Empty line for readability in the command line
     println!("");
 }
